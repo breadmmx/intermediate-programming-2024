4374ae49cb59dc7bbce15d8064b0269a784fb6f7
Tomáš Rohlínek <rohlinektomas@gmail.com>
2024-10-31 16:00:46 +0100

    add week05

---
+++ week05/excercises.cpp
@@ -0,0 +1,20 @@
+// TODO: Doplňte potřebné hlavičky
+
+// Vypište obdélník o zadaných parametrech
+// Vraťte 0 pokud vše v pořádku, -1 pokud nastala chyba
+int hollow_rect(int x, int y, int thickness) {
+    return -1;
+}
+
+// Vraťte a na n
+int power(int a, int n) {
+    return 0;
+}
+
+// Vraťte idx-té fibonacciho číslo
+int fibonacci(int idx) {
+    return 0;
+}
+
+int main() {
+}
