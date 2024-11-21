f9d2a91a4255cdd5fed1f88cc49b0ac1d0faf9bd
oven <kraksna.s.r.o@gmail.com>
2024-11-21 15:49:57 +0100

    sus-06

--- week05/excercises.cpp
+++ week05/excercises.cpp
@@ -1,14 +1,21 @@
+
+#include <iostream>
 // TODO: Doplňte potřebné hlavičky
 
 // Vypište obdélník o zadaných parametrech
 // Vraťte 0 pokud vše v pořádku, -1 pokud nastala chyba
 int hollow_rect(int x, int y, int thickness) {
+    if(x<thickness)
     return -1;
 }
 
 // Vraťte a na n
 int power(int a, int n) {
-    return 0;
+    int ans=1;
+    for( int i=1; i<= n ; i++ ){
+        ans=ans*a;
+    }
+    return ans;
 }
 
 // Vraťte idx-té fibonacciho číslo
@@ -17,4 +24,7 @@ int fibonacci(int idx) {
 }
 
 int main() {
+    
+    std::cout<<power(2, 8);
+    std::cout <<  std::endl;
 }
